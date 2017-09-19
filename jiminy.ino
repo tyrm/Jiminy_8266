9ddeb5e0ce18cfab6fd04e7741b3119b9b091573
Wolfy Meier <wolfy@wlfy.it>
2017-09-18 23:30:36 -0700

    some progress

---
+++ jiminy.ino
@@ -0,0 +1,68 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Connect to the WiFi
+const char* ssid = "ssid";
+const char* password = "password";
+const char* mqtt_server = "mqtt";
+
+const char* my_topic = "/jiminy/c/jewel";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const byte ledPin = 0; // Pin with LED on Adafruit Huzzah
+
+void setup() {
+  Serial.begin(115200);
+
+  for(uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    char receivedChar = (char)payload[i];
+    Serial.print(receivedChar);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266 Client")) {
+      Serial.println("connected");
+       // ... and subscribe to topic
+       client.subscribe("/jiminy/c/all");
+       client.subscribe(my_topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
