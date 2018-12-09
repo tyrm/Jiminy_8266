ec22e737032ba428700a12655b663550059c4981
Tyr Mactire <tyr@pettingzoo.co>
2018-12-09 07:53:29 -0800

    working except MQTTHost

---
+++ mqtt.ino
@@ -0,0 +1,64 @@
+
+void MQTTLoop() {
+  if (!MQTTClient.connected()) {
+    MQTTReconnect();
+  }
+  MQTTClient.loop();
+}
+
+void MQTTPong() {
+  String message = String("<PONG|" + MyName + "|" + LEDCount + "|" + WiFi.localIP() + ">");
+
+  int bufferSize = message.length() + 1;
+  char msgBuffer[bufferSize];
+  message.toCharArray(msgBuffer, bufferSize);
+  
+  MQTTClient.publish("/jiminy/reply", msgBuffer);
+}
+
+void MQTTReconnect() {
+  // Loop until we're reconnected
+  while (!MQTTClient.connected()) {
+    
+    int bufferSize = MyName.length() + 1;
+    char nameBuffer[bufferSize];
+    MyName.toCharArray(nameBuffer, bufferSize);
+
+    bufferSize = sizeof(MQTTUsername) + 1;
+    char usernameBuffer[bufferSize];
+    MQTTUsername.toCharArray(usernameBuffer, bufferSize);
+    
+    bufferSize = sizeof(MQTTPassword) + 1;
+    char passwordBuffer[bufferSize];
+    MQTTPassword.toCharArray(passwordBuffer, bufferSize);
+    
+    Serial.print("Attempting MQTT connection [");
+    Serial.print(usernameBuffer);
+    Serial.print("/");
+    Serial.print(passwordBuffer);
+    Serial.print("] ...");
+    
+    // Attempt to connect
+    if (MQTTClient.connect(nameBuffer, usernameBuffer, passwordBuffer)) {
+      Serial.println("connected");
+       // ... and subscribe to topic
+       MQTTClient.subscribe("/jiminy/c/all");
+       String myTopic = String("/jiminy/c/" + MyName);
+
+       int bufferSize = myTopic.length() + 1;
+       char topicBuffer[bufferSize];
+       myTopic.toCharArray(topicBuffer, bufferSize);
+       
+       MQTTClient.subscribe(topicBuffer);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(MQTTClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+
+  // Let server know we're here
+  MQTTPong();
+}
--- util.ino
+++ util.ino
@@ -8,6 +8,172 @@ String FormatHex8(uint8_t data) {
   return value;
 }
 
+String GetName() {
+  byte mac[6];
+  WiFi.macAddress(mac);
+
+  return String(FormatHex8(mac[5]) + FormatHex8(mac[4]) + FormatHex8(mac[3]) + FormatHex8(mac[2]) + FormatHex8(mac[1]) + FormatHex8(mac[0]));
+}
+
+void ReadConfig() {
+    EEPROM.begin(512);
+  
+  // ** Confirm Header
+  bool Failure = false;
+  uint8_t ExpectedHeader[] = {0x4A, 0x49, 0x4D, 0x01};
+  for (int i = 0; i < 4; i++) {
+    uint8_t HeaderFrame = EEPROM.read(i);
+    if (HeaderFrame != ExpectedHeader[i])
+      Failure = true;
+  }
+  if (Failure) {
+    Serial.println("Invalid header");
+    while(true);
+  }
+  else {
+    Serial.println("Jiminy header v1 found\nReading Config...");
+  }
+
+  // ** Reading Config **
+  uint16_t ee_cursor = 64;
+  uint8_t ee_len = 0;
+  
+  // [0-1] LED Count
+  uint8_t low = EEPROM.read(4);
+  uint8_t high = EEPROM.read(5);
+  uint8_t count_bytes[] = {low, high};
+  LEDCount = *( uint16_t* ) count_bytes;
+  
+  Serial.print(" LED Count: ");
+  Serial.println(LEDCount, DEC);
+
+  // Colors
+  uint8_t r_red = EEPROM.read(6);
+  uint8_t r_green = EEPROM.read(7);
+  uint8_t r_blue = EEPROM.read(8);
+  uint8_t r_white = EEPROM.read(9);
+  uint8_t r_brightness = EEPROM.read(10);
+  
+  Serial.print(" Starting Color: #");
+  Serial.print(FormatHex8(r_red));
+  Serial.print(FormatHex8(r_green));
+  Serial.print(FormatHex8(r_blue));
+  Serial.println(FormatHex8(r_white));
+  
+  Serial.print(" Starting Brightness: ");
+  Serial.println(r_brightness, DEC);
+  
+  VerifyBreakChar(ee_cursor);
+  ee_cursor++;
+  
+  uint8_t SSIDCount = EEPROM.read(ee_cursor);
+  ee_cursor++;
+
+  VerifyBreakChar(ee_cursor);
+  ee_cursor++;
+
+  Serial.print("\n Found ");
+  Serial.print(SSIDCount, DEC);
+  Serial.println(" wifi passwords.");
+
+  for (int i = 0; i < SSIDCount; i++) {
+    // SSID
+    ee_len = EEPROM.read(ee_cursor);
+    ee_cursor++;
+    
+    char r_ssid[ee_len+1];
+    for (int i = 0; i < ee_len; i++) {
+      r_ssid[i] = char(EEPROM.read(ee_cursor));
+      ee_cursor++;
+    }
+    r_ssid[ee_len] = 0x00;
+    
+    VerifyBreakChar(ee_cursor);
+    ee_cursor++;
+    
+    // Password
+    ee_len = EEPROM.read(ee_cursor);
+    ee_cursor++;
+  
+    char r_password[ee_len+1];
+    for (int i = 0; i < ee_len; i++) {
+      r_password[i] = char(EEPROM.read(ee_cursor));
+      ee_cursor++;
+    }
+    r_password[ee_len] = 0x00;
+    
+    VerifyBreakChar(ee_cursor);
+    ee_cursor++;
+
+    WMulti.addAP(r_ssid, r_password);
+  }
+  
+
+  // MQTT Server Config
+  // Host
+  ee_len = EEPROM.read(ee_cursor);
+  ee_cursor++;
+  
+  char r_host[ee_len];
+  for (int i = 0; i < ee_len; i++) {
+    r_host[i] = char(EEPROM.read(ee_cursor));
+    ee_cursor++;
+  }
+  MQTTHost = r_host;
+  
+  VerifyBreakChar(ee_cursor);
+  ee_cursor++;
+  
+  // Username
+  ee_len = EEPROM.read(ee_cursor);
+  ee_cursor++;
+  
+  char r_username[ee_len];
+  for (int i = 0; i < ee_len; i++) {
+    r_username[i] = char(EEPROM.read(ee_cursor));
+    ee_cursor++;
+  }
+  MQTTUsername = r_username;
+  
+  VerifyBreakChar(ee_cursor);
+  ee_cursor++;
+  
+  // Password
+  ee_len = EEPROM.read(ee_cursor);
+  ee_cursor++;
+  
+  char r_password[ee_len];
+  for (int i = 0; i < ee_len; i++) {
+    r_password[i] = char(EEPROM.read(ee_cursor));
+    ee_cursor++;
+  }
+  MQTTPassword = r_password;
+  
+  VerifyBreakChar(ee_cursor);
+  ee_cursor++;
+
+  // [11-12] mqtt_port
+  low = EEPROM.read(11);
+  high = EEPROM.read(12);
+  uint8_t mqtt_port_bytes[] = {low, high};
+  MQTTPort = *( uint16_t* ) mqtt_port_bytes;
+  
+  Serial.print("\n MQTT Server: ");
+  Serial.print(MQTTHost);
+  Serial.print(":");
+  Serial.println(MQTTPort, DEC);
+  
+  Serial.print("  Login: ");
+  Serial.print(MQTTUsername);
+  Serial.print("/");
+  Serial.println(MQTTPassword);
+
+  Serial.println("Successfully Read Config...\n");
+  EEPROM.end();
+
+  InitPixels(LEDCount, r_red, r_green, r_blue, r_white, r_brightness);
+}
+
 void ToggleLED() {
   static boolean ledState = false;
   
